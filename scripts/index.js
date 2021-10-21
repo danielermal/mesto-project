a7d1a45d32d6ba375cedce7171ba9f4f822ac4b7
Daniil Ermalyuk <ermalyukd@yandex.ru>
2021-10-22 01:08:39 +0700

    испр.2

--- scripts/index.js
+++ scripts/index.js
@@ -14,71 +14,72 @@ const photoLinkInput = formPhoto.querySelector('#link')
 const popupImage = document.querySelector('.popup__image')
 const popupImageTitle = document.querySelector('.popup__image-title')
 const popupPhotoCloseButton = document.querySelector('.popup__close_photo')
-const popups = document.querySelectorAll(".popup");
+const popupProfile = document.querySelector('.popup_profile')
+const popupAddPhoto = document.querySelector('.popup_add-photo')
+const popupPhoto = document.querySelector('.popup_photo')
 
-function openPopup(index){
-  popups[index].classList.add("popup_opened");
+function openPopup(popup){
+  popup.classList.add("popup_opened");
 }
 
-function closePopup(index){
-  popups[index].classList.remove("popup_opened");
+function closePopup(popup){
+  popup.classList.remove("popup_opened");
 }
 
-editButton.addEventListener( 'click', ()=> openPopup(0) );
-addButton.addEventListener( 'click', ()=> openPopup(1) );
-closeProfile.addEventListener( 'click', ()=> closePopup(0) );
-closeAddPhoto.addEventListener( 'click', ()=> closePopup(1) );
+editButton.addEventListener( 'click', ()=> openPopup(popupProfile) );
+addButton.addEventListener( 'click', ()=> openPopup(popupAddPhoto) );
+closeProfile.addEventListener( 'click', ()=> closePopup(popupProfile) );
+closeAddPhoto.addEventListener( 'click', ()=> closePopup(popupAddPhoto) );
 
 // Замена данных профиля
 function formProfileSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value
   profileJob.textContent = jobInput.value
-  closePopup(0)
+  closePopup(popupProfile)
 }
 formElementProfile.addEventListener('submit', formProfileSubmitHandler);
 
 // Создание карточки с фото
-function addPhoto(place, link) {
-  const photoTemplate = document.querySelector('.photo__template').content;
-  const photoElement = photoTemplate.querySelector('.element').cloneNode(true);
-  photoElement.querySelector('.element__title').textContent = place
-  const photo = photoElement.querySelector('.element__img')
-  photo.src = link
-  photo.alt = place
+function addCard(place, link) {
+  const cardTemplate = document.querySelector('.photo__template').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__title').textContent = place
+  const card = cardElement.querySelector('.element__img')
+  card.src = link
+  card.alt = place
   // ставим лайк
-  photoElement.querySelector('.element__like').addEventListener('click', function(evt) {
+  cardElement.querySelector('.element__like').addEventListener('click', function(evt) {
     evt.target.classList.toggle('element__like_active')
   })
-  closePopup(1)
   // удаляем карточки
-  const deleteButton = photoElement.querySelector('.element__delete')
+  const deleteButton = cardElement.querySelector('.element__delete')
   deleteButton.addEventListener('click', function() {
-    photoElement.remove()
+    cardElement.remove()
   })
   // открываем попап с картинкой
-  photo.addEventListener('click', function(){
-    openPopup(2)
+  card.addEventListener('click', function(){
+    openPopup(popupPhoto)
     popupImage.src = link
     popupImage.alt = place
     popupImageTitle.textContent = place
   })
-  return photoElement
+  return cardElement
 }
 // вставляем карточку
 function renderPhoto(photoPlace, photoLink) {
-  photoContainer.prepend(addPhoto(photoPlace, photoLink))
+  photoContainer.prepend(addCard(photoPlace, photoLink))
 }
 
 // получаем данные для фото
 function formSubmitPhoto (evt) {
   evt.preventDefault()
   renderPhoto(photoPlaceInput.value, photoLinkInput.value)
-  photoPlaceInput.value.reset()
-  photoLinkInput.value.reset()
+  formPhoto.reset()
+  closePopup(popupAddPhoto)
 }
 formPhoto.addEventListener('submit', formSubmitPhoto);
-popupPhotoCloseButton.addEventListener('click', ()=> closePopup(2))
+popupPhotoCloseButton.addEventListener('click', ()=> closePopup(popupPhoto))
 
 // добавляем шесть карточек
 const initialCards = [
