563cec64198e0e3bc22f990109ebf23b5f49d04a
Daniil Ermalyuk <ermalyukd@yandex.ru>
2022-02-03 21:50:28 +0700

    Section

---
+++ src/components/Section.js
@@ -0,0 +1,24 @@
+export default class Section {
+    constructor({ data, renderer }, containerSelector) {
+        this._renderedItems = data;
+        this._renderer = renderer;
+        this._container = document.querySelector(containerSelector);
+    }
+
+    setItem(element) {
+        this._container.append(element);
+    }
+
+    clear() {
+        this._container.innerHTML = '';
+    }
+
+    addItems() {
+        this.clear();
+
+        this._renderedItems.forEach((item) => {
+            this._renderer(item);
+        });
+    }
+
+}
